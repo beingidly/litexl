240a81b482224c84fe1a3a052bae0a93fafdc8c8
Hyeonsik Song <hssong@beingidly.com>
2026-03-22 20:56:41 +0900

    Add Maven Central publishing to litexl-chart and bump version to 0.1.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "com.beingidly"
-version = "0.1.9"
+version = "0.1.10"
 
 java {
     toolchain {
--- litexl-chart/build.gradle.kts
+++ litexl-chart/build.gradle.kts
@@ -1,15 +1,17 @@
 plugins {
     java
     `java-library`
+    alias(libs.plugins.maven.publish)
 }
 
 group = "com.beingidly"
-version = "0.1.9"
+version = "0.1.10"
 
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(libs.versions.java.get().toInt())
     }
+    withSourcesJar()
 }
 
 repositories {
@@ -33,4 +35,42 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+// Maven Central publishing via vanniktech plugin
+mavenPublishing {
+    configure(com.vanniktech.maven.publish.JavaLibrary(
+        javadocJar = com.vanniktech.maven.publish.JavadocJar.Javadoc(),
+        sourcesJar = com.vanniktech.maven.publish.SourcesJar.Sources()
+    ))
+    publishToMavenCentral()
+    signAllPublications()
+
+    pom {
+        name = "LiteXL Chart"
+        description = "Chart and graph support for LiteXL, a lightweight Excel (XLSX) library for Java"
+        url = "https://github.com/beingidly/litexl"
+        inceptionYear = "2024"
+
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+
+        developers {
+            developer {
+                id = "hssong"
+                name = "Hyeonsik Song"
+                email = "hssong@@beingidly.com"
+            }
+        }
+
+        scm {
+            connection = "scm:git:git://github.com/beingidly/litexl.git"
+            developerConnection = "scm:git:ssh://github.com/beingidly/litexl.git"
+            url = "https://github.com/beingidly/litexl"
+        }
+    }
 }
